2b27d0d98759f082349a4a24ddbf42b819299881
Inna Zlotska <58848421+ururh@users.noreply.github.com>
2024-03-06 09:29:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
-  <script src="https://event.webinarjam.com/register/7y8y6sm/embed-bar?buttonText=Register&buttonBgColor=%23FFFFFF&buttonBgOpacity=1&barBgColor=%2329b6f6&barBgOpacity=0.95&formTemplate=4&formColor=1"></script>
     </BrowserRouter>
   </React.StrictMode>
 );
